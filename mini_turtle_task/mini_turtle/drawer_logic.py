52ae0eb67175df4042a2b52e6f5dd2477f99079d
ALEJANDRO BEDOYA LOPERA <alejandro.bedoya@est.iudigital.edu.co>
2025-12-15 15:37:03 -0500

    Implement turtle movement and reset functions
    Added functions to control turtle movement and reset position.

--- mini_turtle_task/mini_turtle/drawer_logic.py
+++ mini_turtle_task/mini_turtle/drawer_logic.py
@@ -1,1 +1,20 @@
+import mi_turtle
+
+espacios = 0                                                  
+def adelante (n):
+   global espacios                                             
+   print(espacios * " " + "-" * n + ">" )
+   espacios = espacios + n
+
+def abajo (n):
+  for a in range (n):
+    print(" " * espacios +"|")
+  print (" " * espacios +"↓")
+
+## funcion adicional reiniciar
+
+def reiniciar ():
+  global espacios
+  espacios = 0
+  print ("se reinició la posicion de la tortuga")
 
