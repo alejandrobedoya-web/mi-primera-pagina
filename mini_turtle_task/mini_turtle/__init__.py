425a86bbe48ee235102ddb5d840be930822217c6
ALEJANDRO BEDOYA LOPERA <alejandro.bedoya@est.iudigital.edu.co>
2025-12-15 15:46:33 -0500

    Refactor imports in __init__.py

--- mini_turtle_task/mini_turtle/__init__.py
+++ mini_turtle_task/mini_turtle/__init__.py
@@ -1,3 +1,4 @@
-from drawer_logic import (
-
-
+from drawer_logic import ( adelante, abajo, reiniciar)
+## version y lista de elementos exportados
+__version__ = 1.0.0
+__all__ = ["adelante", "abajo", "reiniciar"]
